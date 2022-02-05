c0ce5c7e4c44c4f51240f4bbe292f5ca1db73af2
Cedric <cedric.lphn@yahoo.com>
2022-02-05 23:29:44 +0100

    feat: upload

--- lib/infrastructure/s3_upload.dart
+++ lib/infrastructure/s3_upload.dart
@@ -1,26 +1,30 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 import 'dart:typed_data';
+import 'package:dio/dio.dart';
 
-import 'package:minio/minio.dart';
+class Storage {
+  Dio dio = Dio();
 
+  Future<dynamic> getPresignatedURL() async {
+    FormData formData = FormData();
 
-class Storage{
-  final minio = Minio(
-    endPoint: 's3.eu-west-1.amazonaws.com',
-    accessKey: '',
-    secretKey: '',
-  );
+    print("hello");
 
-  void add(Uint8List bits) async{
-    await minio.putObject(
-      'tradesme-bucket',
-      "test.png",
-      Stream.value(bits),
-      onProgress: (bytes) => print('$bytes uploaded'),
-    );
+    try {
+      var response = await dio
+          .get("https://bc9c50wvrj.execute-api.eu-west-1.amazonaws.com/Prod/");
+
+      return response.data["uploadURL"];
+    } catch (e) {
+      print(e);
+    }
+
+    return "";
   }
 
-  void get() async{
+  void get() async {
     // print(await minio.bucketExists('tradesme-bucket'));
     final stream = await minio.getObject('tradesme-bucket', 'bon.png');
 
@@ -28,6 +32,19 @@ class Storage{
     // print(stream.contentLength);
   }
 
+  Future<bool> upload(String bucketUrl, Uint8List data) {
+    Map<String, String> headers = new Map<String, String>();
+    headers.putIfAbsent("Content-Type", () => "image/jpeg");
+    print("sending");
+    bool status = false;
+    Dio dios = Dio();
+    dios.options.contentType = "image/jpeg";
+    return dios
+        .put(bucketUrl,
+            data: data,
+            options: Options(headers: headers))
+        .then((value) => value.statusCode == 200);
+  }
 }
 
 
